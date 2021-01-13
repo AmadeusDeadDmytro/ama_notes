a006c7c0c76ac94518a76729c23ffc1325d4fcd5
Amadeus <mazurenkodmitriy57@gmail.com>
2021-01-13 11:22:32 +0200

    bugfix: adding new note from keys from trash

--- src/containers/KeyboardShortcuts.tsx
+++ src/containers/KeyboardShortcuts.tsx
@@ -27,7 +27,7 @@ const KeyboardShortcuts: React.FC<KeyboardShortcutsProps> = ({ activeNote, activ
     const newNoteHandler = () => {
         const note = newNote(activeCategoryId, activeFolder)
 
-        if ((activeNote && activeNote.text !== '') || !activeNote) {
+        if ((activeNote && activeNote.text !== '' && !activeNote.trash) || !activeNote) {
             addNote(note)
             swapNote(note.id)
         }
